e6a2a567767e1bb169c8364c29d5a319e99d4ec0
vaahu <97781309+vaahu@users.noreply.github.com>
2023-04-15 00:33:55 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,13 @@
+package ATM;
+
+import java.io.IOException;
+
+public class ATM extends optionmenu {
+
+	public static void main(String[] args) throws IOException {
+		optionmenu optionMenu = new optionmenu();
+
+		optionMenu.getLogin();
+	}
+
+};
